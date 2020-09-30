81625e13dc55aeff4083756ef40298553acfcc9c
Douglas Vinicio <douglasvinicio@Douglas-MacBook-Pro.local>
2020-09-30 18:25:50 -0400

    Update

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    
+    //Text that might change in the future.
+    var introductionText = "I'm a 28 years old guy from Brazil that loves learning and discovering new technologies " +
+    "I'm learning how to code to have a chance to be part of something bigger, bieng able to help developing tools that will be usefull for ourselves and future generations."
+
+   // Set text
+   $("#introduction-text").text(introductionText);
+});
